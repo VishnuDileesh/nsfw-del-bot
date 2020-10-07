859376ae3519066e41787d95b6837de11b3b80bd
Vishnu Dileesh <dev@vishnudileesh.com>
2020-10-07 20:18:30 +0530

    Add - deepai & feature - bot delete nsfw images

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const client = new Discord.Client()
 
 const config = require('dotenv').config()
 
+const deepai = require('deepai')
+
+deepai.setApiKey(process.env.AI_API_KEY)
+
 
 client.on('ready', () => {
 
@@ -13,8 +17,42 @@ client.on('ready', () => {
       channel.send("Am watching you...")
     })
 
+})
 
+
+client.on('message', (msg) => {
+
+  if(msg.author.bot) return
+
+  if(msg.attachments.size > 0){
+    msg.attachments.forEach((image) => {
+      const imageUrl = image.url
+
+      filterImage(imageUrl, msg)
+
+    })
+  }
 
 })
+
+
+function filterImage(url, msg){
+  
+    deepai.callStandardApi('nsfw-detector', {
+      image: url
+    })
+    .then((result) => {
+      let score = result.output.nsfw_score
+      let nsfwScore = score.toFixed(2)
+
+      if(nsfwScore > 0.45){
+        msg.delete()
+      }
+
+    })
+    .catch((err) => console.log(err))
+
+}
+
 
 client.login(process.env.BOT_TOKEN)
